dcc589824438763ebd616616c7df271606c04020
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-07-13 00:36:47 +0200

    added flex_middle complex flexbox system

---
+++ frontend/src/components/flex/flex_middle.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+    props: ['gap', 'dir', 'fill'],
+    data() {
+        return {
+            width: 'auto',
+            height: 'auto',
+        }
+    },
+    computed: {
+        cssVars () {
+            return {
+                '--direction': (this.dir ? this.dir : 'column'),
+                '--width': this.width,
+                '--height': this.height,
+                '--gap': (this.gap) + (isNaN(this.gap)?'':"px"),
+            }
+        }
+    },
+    mounted:function(){
+            this.getFill()
+    },
+    methods: {
+        getFill() {
+            if (typeof(this.fill) == 'object') {
+                this.setFill('x', this.fill.x);
+                this.setFill('y', this.fill.y);
+            }
+            else
+            {
+                this.setFill('x', this.fill);
+                this.setFill('y', this.fill);
+            }
+        },
+        setFill(dimension, type) {
+            let value = 'undefined';
+            if (
+                typeof(type) == 'undefined' ||
+                type == 'fill'
+            ) {
+                value = '100%';
+            }
+            else if (type == 'auto' || type == 'none')
+            {
+                value = 'auto';
+            }
+
+            if (dimension == 'x') {
+                this.width= value;
+            }
+            else if (dimension == 'y') {
+                this.height= value;
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="flex_box" :style="cssVars">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+    div.flex_box{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: var(--direction);
+        gap: var(--gap);
+
+        width: var(--width);
+        height: var(--height);
+    }
+</style>
