ca30d58942a187f35641c5656a882683c43e593f
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-07-13 00:40:32 +0200

    buttons col with flex for mobile

---
+++ frontend/src/components/buttons/buttons_col.vue
@@ -0,0 +1,52 @@
+<script>
+import flexMiddle from '../flex/flex_middle.vue';
+import ButtonComp from './buttons.vue';
+
+export default {
+    components: {
+        ButtonComp,
+        flexMiddle,
+    },
+    props: ['size_of_btns', "screenSize", "gaps_between_btns"],
+    data() {
+        return {
+            items: [
+                { text:"Github" },
+                { text:"Contact" },
+                { text:"Resume" }
+            ],
+        }
+    },
+    computed: {
+        cssVars () {
+            return {
+                '--size_of_btns': (this.size_of_btns) + "px",
+            }
+        },
+        mobile_buttons() {return (this.screenSize.width < (this.size_of_btns * this.items.length + this.gaps_between_btns * (this.items.length + 1)))}
+    }
+}
+</script>
+
+
+<template>
+<flexMiddle :dir="[ mobile_buttons?'column':'row' ]" :fill="'none'" :gap="gaps_between_btns">
+    <ButtonComp
+        :style="cssVars"
+        v-for="item in items"
+        :text="item.text"
+    />
+</flexMiddle>
+</template>
+
+<style scoped>
+button {
+    width:var(--size_of_btns);
+}
+</style>
+<style>
+.flexForMobile {
+    flex-direction: column;
+    gap: 20px;
+}
+</style>
