d26c3194185335dd9d67ca288040f86b3d51c94a
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-07-13 00:41:43 +0200

    vertical lines for footer

---
+++ frontend/src/components/vertical_line.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'vl',
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<style>
+.vl {
+  border-left: 2px solid #0AE1C1;
+  height: 100%;
+}
+</style>
+
+<template>
+    <div class="vl"></div>
+</template>
