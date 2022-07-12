27a23ac0bb8310f7da7bf1a8297554664c171243
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-07-13 00:41:15 +0200

    fill_window for filling entire screen with one component

---
+++ frontend/src/components/fill_window.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    props: ['height'],
+    data() {
+        return {
+        }
+    },
+    computed: {
+        varsCSS() {
+            return {
+                '--height': (this.height) ? this.$props.height : '100vh'
+            }
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="fill_window" :style="varsCSS">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+div.fill_window {
+
+    width: 100%;
+    height: var(--height);
+}
+</style>
