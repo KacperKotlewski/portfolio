5c685939d16080a2ffef877aa912eb0e153dd420
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-07-14 19:50:17 +0200

    added bl;ue text component

---
+++ frontend/src/components/text/blue.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+    name: 'blue_text',
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+
+<template>
+    <span class="blue_text">
+        <slot/>
+    </span>
+</template>
+
+<style>
+span.blue_text {
+    color: #0AE1C1;
+}
+</style>
