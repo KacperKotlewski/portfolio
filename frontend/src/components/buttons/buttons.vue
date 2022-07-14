0623f4dc3767bc7cf47ff38db221bd9ee2e01dbe
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-07-14 23:05:53 +0200

    added link and target possibility, changed items to display hidden buttons: -contact -resume new button: -repo

--- frontend/src/components/buttons/buttons.vue
+++ frontend/src/components/buttons/buttons.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  props: ['text', 'icon'],
+  props: ['text', 'icon', 'link', 'target'],
   data() {
     return {
     }
@@ -9,11 +9,11 @@ export default {
 </script>
 
 <template>
-    <button><i :class='icon'></i> {{ text }}</button>
+    <a :href='link' :target="target"><i :class='icon'></i> {{ text }}</a>
 </template>
 
 <style scoped>
-    button {
+    a {
         background-color: rgba(0, 0, 0, 0.8);
         border: 3px solid #0AE1C1;
         border-radius: 1rem;
@@ -22,8 +22,9 @@ export default {
         padding: 0.5rem 1rem;
         transition: 0.4s;
         text-align: center;
+        text-decoration: none;
     }
-    button:hover {
+    a:hover {
 
         border: 0.2rem solid #fff;
         border-radius: 1.2rem;
