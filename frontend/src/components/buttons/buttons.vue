00b507b083b3b2f930d15ced808816d7f4b73c93
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-07-22 22:46:40 +0200

    changed buttons to fit new design

--- frontend/src/components/buttons/buttons.vue
+++ frontend/src/components/buttons/buttons.vue
@@ -9,31 +9,52 @@ export default {
 </script>
 
 <template>
-    <a :href='link' :target="target"><i :class='icon'></i> {{ text }}</a>
+    <a :href='link' :target="target"><i v-if="icon" :class='icon'></i>&nbsp;<span v-if="text">{{ text }}</span></a>
 </template>
 
 <style scoped>
     a {
-        background-color: rgba(0, 0, 0, 0.8);
-        border: 3px solid #0AE1C1;
-        border-radius: 1rem;
         color: #0AE1C1;
         font-size: 1.2rem;
         padding: 0.5rem 1rem;
         transition: 0.4s;
-        text-align: center;
+        text-align: left;
         text-decoration: none;
+        position: relative;
     }
-    a:hover {
-
-        border: 0.2rem solid #fff;
-        border-radius: 1.2rem;
-        padding: 0.4em;
-        box-shadow: 0 0 .1rem #fff,
-                    0 0 .1rem #fff,
-                    0 0 1rem #0AE1C1,
-                    0 0 0.2rem #0AE1C1,
-                    0 0 1.6rem #0AE1C1,
-                    inset 0 0 0.65rem #0AE1C1;
+    a::before{
+        content: '';
+        display: block;
+        width: 2em;
+        height: 60%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: rgba(0,0,0,0.8);
+        transform: translate(2em, 0.5em);
+        transition: all 0.3s ease-in;
+    }
+    a::after{
+        content: '';
+        display: block;
+        width: 100%;
+        height: 1%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        border: 3px solid #0ae1c100;
+        transform: translate(0em, 1em);
+        transition: all 0.2s;
+    }
+    a:hover::before {
+      width: 100%;
+      height: 100%;
+      transform: translate(0em, 0em);
+    }
+    a:hover::after {
+      width: 100%;
+      height: 100%;
+      transform: translate(0em, 0em);
+      border-color: #0AE1C1;
     }
 </style>
