29ed5fbe7eea22c0e1510dc7028e3bf8c3e85efe
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-07-22 23:14:28 +0200

    added constant button class to <a>

--- frontend/src/components/buttons/buttons.vue
+++ frontend/src/components/buttons/buttons.vue
@@ -9,14 +9,14 @@ export default {
 </script>
 
 <template>
-    <a :href='link' :target="target">
+    <a :href='link' :target="target" class="button">
       <div class="place_holder_in_button"><i v-if="icon" class="bi" :class='icon'></i>&nbsp;<span v-if="text">{{ text }}</span></div>
       <div class="text_in_button"><i v-if="icon" class="bi" :class='icon'></i>&nbsp;<span v-if="text">{{ text }}</span></div>
     </a>
 </template>
 
 <style scoped>
-    a {
+    a.button {
         color: #0AE1C1;
         font-size: 1rem;
         transition: 0.4s;
@@ -24,7 +24,7 @@ export default {
         text-decoration: none;
         position: relative;
     }
-    a::before{
+    a.button::before{
         content: '';
         display: block;
         width: 2em;
@@ -36,7 +36,7 @@ export default {
         transform: translate(2em, 0.5em);
         transition: all 0.3s ease-in;
     }
-    a::after{
+    a.button::after{
         content: '';
         display: block;
         width: 80%;
@@ -48,18 +48,18 @@ export default {
         transform: translate(0.5em, 0.2em);
         transition: all 0.2s;
     }
-    a:hover::before {
+    a.button:hover::before {
       width: 100%;
       height: 100%;
       transform: translate(0em, 0em);
     }
-    a:hover::after {
+    a.button:hover::after {
       width: 100%;
       height: 100%;
       transform: translate(0em, 0em);
       border-color: #0AE1C1;
     }
-    a > div{
+    a.button > div{
       padding: 0.5em 1em;
       width: 100%;
       height: 100%;
@@ -68,19 +68,19 @@ export default {
       align-items: center;
       justify-content: left;
     }
-    a > div.text_in_button{
+    a.button > div.text_in_button{
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
       position: absolute;
     }
-    a:hover > div.text_in_button{
+    a.button:hover > div.text_in_button{
       font-size: 1.2em;
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
     }
-    a > div.place_holder_in_button{
+    a.button > div.place_holder_in_button{
         color: #0ae1c100;
     }
 </style>
