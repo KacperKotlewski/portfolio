ea91775389cdcf6b33836f7ba71cd73342994898
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-05-07 19:43:53 +0200

    updated tailwind styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },  
-  plugins: [],
-}
 
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontSize: {
+        "em-xs": ".75em",
+        "em-s": ".87em",
+      },
+      colors: {
+        "highlight": "var(--color-highlight)",
+        "highlight-muted": "var(--color-highlight-muted)",
+        "primary-bg": "var(--color-background)",
+        "primary-bg-muted": "var(--color-background-muted)",
+        "primary-bg-hard": "var(--color-background-hard)",
+        "primary": "var(--color-text)",
+        "primary-muted": "var(--color-text-muted)",
+      }
+    },
+  },
+  plugins: [],
+  safelist: [],
+};
