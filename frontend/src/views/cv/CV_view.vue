ff023db574121e43249c7fe11146968250c75f39
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-05-07 19:44:11 +0200

    cv hard update

--- frontend/src/views/cv/CV_view.vue
+++ frontend/src/views/cv/CV_view.vue
@@ -3,33 +3,97 @@ import navbar_on_side from "../../components/sections/navbar_on_side.vue";
 
 import vue_footer from "../../components/sections/footer.vue";
 
-import { registerRuntimeHelpers } from "@vue/compiler-core";
+import custom_button from "../../components/buttons/buttons.vue";
+
+import CV_component from "./components/cv.vue";
 
 export default {
   props: ["screenSize"],
   components: {
     navbar_on_side,
-
+    custom_button,
     vue_footer,
+
+    CV_component,
   },
   data() {
     return {
+      btns: [
+        {
+          text: "zoom in",
+          icon: "bi-zoom-in",
+          action: () => {
+            this.zoomIn();
+          },
+        },
+        {
+          text: "restart zoom",
+          icon: "bi-arrows-angle-contract",
+          action: () => {
+            this.zoomScale = 1;
+            this.applyNewScale();
+          },
+        },
+        {
+          text: "zoom out",
+          icon: "bi-zoom-out",
+          action: () => {
+            this.zoomOut();
+          },
+        },
+        {
+          text: "download",
+          icon: "bi-download",
+          link: "/cv/download",
+          target: "_blank",
+        },
+      ],
+      zoomScale: 1,
     };
   },
-  computed: {
+  mounted() {},
+  computed: {},
+  methods: {
+    zoomIn() {
+      this.zoomScale += 0.1;
+      if (this.zoomScale > 2) this.zoomScale = 2;
+      this.applyNewScale();
+    },
+    zoomOut() {
+      this.zoomScale -= 0.1;
+      if (this.zoomScale < 0.5) this.zoomScale = 0.5;
+      this.applyNewScale();
+    },
+    applyNewScale() {
+      document.documentElement.style.setProperty(
+        "--a4-page-size",
+        `calc(100vh * ${this.zoomScale})`
+      );
+    },
   },
-  methods: {},
 };
 </script>
 
 <template>
   <navbar_on_side :views="null" />
-  <div>
-    
+  <div
+    class="fixed bottom-0 right-4 overflow-hidden w-fit h-fit z-10 bg-primary-bg-hard"
+  >
+    <template v-for="btn in btns" :key="btn.text">
+      <div
+        class="text-highlight-muted hover:text-highlight hover:scale-125 p-2"
+        @click="btn.action"
+      >
+        <i class="bi text-2xl" :class="btn.icon"></i>
+      </div>
+    </template>
+  </div>
+
+  <div
+    class="page-container min-h-full w-full flex flex-row justify-center item-center flex-wrap gap-4"
+  >
+    <CV_component />
   </div>
   <!-- <in_build id="slide_1" class="blockedElement"/> -->
-  <vue_footer class="blockedElement hiddenElement" />
+  <vue_footer class="blockedElement" />
 </template>
-
-<style lang="scss">
-</style>
---
+++ frontend/src/views/cv/components/cv.vue
@@ -0,0 +1,208 @@
+<script setup>
+</script>
+
+<template>
+  <div class="page flex">
+    <section
+      class="px-4 py-12 bg-slate-800 text-gray-300 h-full w-4/12 text-center"
+    >
+      <h2>Kacper Kotlewski</h2>
+      <span>Web Software developer</span>
+      <article>
+        <h3>Profile</h3>
+        <p>
+          22 years old software developer,<br />
+          10 years of passion in programming,<br />
+          4 years as freelance <br />
+          2 years as worker
+        </p>
+      </article>
+      <article>
+        <h3>Contact</h3>
+        <p>
+          <a href="mailto:kacperkotlewski.kontakt@gmail.com" class="text-em-s">
+            kacperkotlewski.kontakt@gmail.com
+          </a>
+
+          <!-- phone: <a href="tel:+48123456789">+48 123 456 789</a> -->
+        </p>
+        <p>
+          <span>
+            <strong>Ruda Śląska</strong>, Katowice, Gliwice, Częstochowa
+          </span>
+          <span class="opacity-60">, <strong>Poland</strong> </span>
+        </p>
+      </article>
+      <article>
+        <h3>Languages</h3>
+        <ul>
+          <li><strong>Polish</strong> - Native</li>
+          <li><strong>English</strong> - B2</li>
+        </ul>
+        <p>Additionally I have an very basic knowledge of:</p>
+        <ul>
+          <li>German - A1/A2</li>
+          <li>Chinese - HSK1</li>
+          <li>Italian - mostly food names😅</li>
+          <li>Japanese - below N5</li>
+        </ul>
+      </article>
+    </section>
+    <section class="px-4 py-12 bg-slate-100 h-full flex-1">
+      <article>
+        <h3>Work experience</h3>
+        <ul>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+        </ul>
+        <h3>Work experience</h3>
+        <ul>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+        </ul>
+        <h3>Work experience</h3>
+        <ul>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+          <li>
+            <div class="flex flex-row justify-between">
+              <h4>title</h4>
+              <span>mm/yyyy - mm/yyyy</span>
+            </div>
+            <span>desc</span>
+          </li>
+        </ul>
+      </article>
+    </section>
+  </div>
+</template>
+<style lang="scss">
+:root {
+  --a4-page-size: 100vh;
+}
+</style>
+<style scoped lang="scss">
+$a4_aspect_ratio: 1.414;
+$text_ratio_to_vh: 0.0133;
+$text_size: calc(var(--a4-page-size) * $text_ratio_to_vh);
+
+div.page {
+  background-color: #fff;
+  color: #000;
+  font-size: $text_size;
+  height: var(--a4-page-size);
+  width: calc(var(--a4-page-size) / $a4_aspect_ratio);
+}
+
+p {
+  margin: 1em 0;
+}
+.page > article {
+  margin: 1em 0;
+}
+
+a {
+  color: var(--color-highligh) !important;
+  text-decoration: underline;
+}
+li {
+  margin: 0.3em 0;
+}
+
+h2,
+h3,
+h4 {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 2em;
+}
+
+article:not(:first-child) {
+  margin: 1em 0;
+}
+
+h3 {
+  &:not(:first-child) {
+    margin-top: 1em;
+  }
+
+  font-size: 1.5em;
+  text-decoration: solid underline 2px;
+}
+h4 {
+  font-size: 1.2em;
+}
+</style>
