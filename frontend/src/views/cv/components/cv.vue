27d3fdb9939ac680f5a6cf88e9c39e3a6a5be177
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-05-08 21:11:30 +0200

    updated whole CV

--- frontend/src/views/cv/components/cv.vue
+++ frontend/src/views/cv/components/cv.vue
@@ -1,150 +1,246 @@
 <script setup>
+const agreement_text = "I hereby consent to the processing of my personal data for the purposes necessary to carry out the recruitment process (in accordance with the Act of 10 May 2018 on the protection of personal data (Journal of Laws of 2018, item 1000) and in accordance with the Regulation of the European Parliament and of the Council (EU) 2016/679 of 27 April 2016 on the protection of individuals with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (GDPR)).";
 </script>
 
 <template>
-  <div class="page flex">
-    <section
-      class="px-4 py-12 bg-slate-800 text-gray-300 h-full w-4/12 text-center"
-    >
-      <h2>Kacper Kotlewski</h2>
-      <span>Web Software developer</span>
-      <article>
-        <h3>Profile</h3>
-        <p>
-          22 years old software developer,<br />
-          10 years of passion in programming,<br />
-          4 years as freelance <br />
-          2 years as worker
-        </p>
-      </article>
-      <article>
-        <h3>Contact</h3>
-        <p>
-          <a href="mailto:kacperkotlewski.kontakt@gmail.com" class="text-em-s">
-            kacperkotlewski.kontakt@gmail.com
-          </a>
-
-          <!-- phone: <a href="tel:+48123456789">+48 123 456 789</a> -->
-        </p>
+  <div class="w-fit h-fit">
+    <div class="page flex">
+      <section
+        class="px-0 py-6 slate-bg text-gray-300 h-full w-4/12 text-center flex flex-col justify-around items-center"
+      >
+        <h2>Kacper Kotlewski</h2>
+        <span>Web Software developer</span>
         <p>
           <span>
             <strong>Ruda Śląska</strong>, Katowice, Gliwice, Częstochowa
           </span>
           <span class="opacity-60">, <strong>Poland</strong> </span>
         </p>
-      </article>
-      <article>
-        <h3>Languages</h3>
-        <ul>
-          <li><strong>Polish</strong> - Native</li>
-          <li><strong>English</strong> - B2</li>
-        </ul>
-        <p>Additionally I have an very basic knowledge of:</p>
-        <ul>
-          <li>German - A1/A2</li>
-          <li>Chinese - HSK1</li>
-          <li>Italian - mostly food names😅</li>
-          <li>Japanese - below N5</li>
-        </ul>
-      </article>
-    </section>
-    <section class="px-4 py-12 bg-slate-100 h-full flex-1">
-      <article>
-        <h3>Work experience</h3>
-        <ul>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-        </ul>
-        <h3>Work experience</h3>
-        <ul>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-        </ul>
-        <h3>Work experience</h3>
-        <ul>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-          <li>
-            <div class="flex flex-row justify-between">
-              <h4>title</h4>
-              <span>mm/yyyy - mm/yyyy</span>
-            </div>
-            <span>desc</span>
-          </li>
-        </ul>
-      </article>
-    </section>
+        <hr />
+        <!-- profile -->
+        <article>
+          <h3>Profile</h3>
+          <p>
+            22 years old software developer👨,<br />
+            9 years of passion in programming💻,<br />
+            3 years as freelance🐬, <br />
+            2 years as worker💼, <br />
+            2 years as team leader in students projects⚓
+          </p>
+          <p>
+            6 years of HTML & CSS, <br />
+            5 years of Javascript, <br />
+            4 years of Python, <br />
+            3 years of Django, <br />
+            2 years of REST API, <br />
+            1 year of Vue.js & TypeScript, <br />
+            1 year of unit Testing
+          </p>
+        </article>
+        <hr />
+        <!-- contact -->
+        <article>
+          <h3>Connect</h3>
+          <p>
+            <a
+              href="https://kacperkotlewski.pl/"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-em-s underline text-highlight-muted"
+            >
+              kacperkotlewski.pl
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kacper-kotlewski/"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-em-s underline text-highlight-muted"
+            >
+              www.linkedin.com/in/kacper-kotlewski
+            </a>
+            <a
+              href="mailto:kacperkotlewski.kontakt@gmail.com"
+              class="text-em-s underline text-highlight-muted"
+            >
+              kacperkotlewski.kontakt@gmail.com
+            </a>
+
+            <!-- phone: <a href="tel:+48123456789">+48 123 456 789</a> -->
+          </p>
+        </article>
+        <hr />
+        <!-- languages -->
+        <article>
+          <h3>Languages</h3>
+          <ul>
+            <li><strong>Polish</strong> - Native</li>
+            <li><strong>English</strong> - B2</li>
+          </ul>
+          <p>Additionally I have an very basic knowledge of:</p>
+          <ul>
+            <li>German - A1/A2</li>
+            <li>Chinese - HSK1</li>
+            <li>Italian - mostly food names😅</li>
+            <li>Japanese - below N5</li>
+          </ul>
+        </article>
+        <!-- Interests -->
+        <article>
+          <h3>Interests/Hobbies</h3>
+          <p class="px-4">
+            My GF💘, Marvel/Superheros🦸, movies/series🎬, Travels✈️, Infinity
+            growth🚩, Self improvement🧩, Investments💵, Business💡, Tools🪛,
+            AI🤖, Programming🧑‍💻, Web development📄, Web design🎆, UI/UX🖼️.
+          </p>
+        </article>
+      </section>
+      <section class="px-4 py-6 h-full flex-1 flex flex-col justify-between">
+        <div class="w-full h-fit flex-1 flex flex-col justify-between pb-12">
+          <!-- 
+          experience
+         -->
+          <article>
+            <h3>Work experience</h3>
+            <ul>
+              <li>
+                <div class="flex flex-row justify-between items-end">
+                  <h4>Junior Software Tester</h4>
+                  <span>09/2022 - now</span>
+                </div>
+                <span class="desc">
+                  Unit testing C programs for car hardware with external tools,
+                  Python scripting, working with documentation, guidelines
+                  procedures, and finding defects, V-model methodology.
+                </span>
+              </li>
+              <li>
+                <div class="flex flex-row justify-between items-end">
+                  <a href="https://inbie.pl/">
+                    <h4>INBIE - Instytut Badań i Innowacji w Edukacji</h4>
+                  </a>
+                  <span>04/2021 - now</span>
+                </div>
+                <span class="desc">
+                  Working on a Erasmus+ projects with foundation. Projects
+                  consider creating a web pages of particular project, helping
+                  with e-learning platform, creating a mobile application,
+                  making presentation, and present in front of other partners
+                  all over the Europe continent.
+                </span>
+              </li>
+              <li>
+                <div class="flex flex-row justify-between items-end">
+                  <h4>Fullstack Python/JS web developer</h4>
+                  <span>01/2020 - now</span>
+                </div>
+                <span class="desc">
+                  Web development using mainly Figma for designs, Docker for
+                  VMs, PostgreSQL as DB, Python/Django (DRF) as a backend, and
+                  Javascript/Typescript + Vue.js as a frontend, kanban.
+                </span>
+              </li>
+              <li>
+                <div class="flex flex-row justify-between items-end">
+                  <h4>IT Teacher</h4>
+                  <span>09/2020 - 07/2021</span>
+                </div>
+                <span class="desc">
+                  Teaching basics of web development, and programming in JS,
+                  HTML, CSS in private IT school.
+                </span>
+              </li>
+              <li>
+                <div class="flex flex-row justify-between items-end">
+                  <h4>Practice as web developer</h4>
+                  <span>3 month in 2019</span>
+                </div>
+                <span class="desc">
+                  Practice at 2 different small companies as a web developer.
+                  HTML, CSS, JS, PHP, MySQL, Wordpress, Moodle, Python,
+                  Networking, Documentation, time management, team leading, and
+                  Scrum methodologies.
+                </span>
+              </li>
+              <li>
+                <div class="flex flex-row justify-between items-end">
+                  <h4>Blacer Ltd</h4>
+                  <span>11/2015 - 05/2019 </span>
+                </div>
+                <span class="desc">
+                  Co-founder of eSport organization. I was responsible for web
+                  pages, eSport events organization, eSport events commentator,
+                  hardware maintenance, and head of Unity Game development team.
+                </span>
+              </li>
+            </ul>
+          </article>
+
+          <article>
+            <h3>Skills</h3>
+            <ul>
+              <li>
+                <h4>Main language and frameworks/libraries</h4>
+                <span class="desc">
+                  Python, Django, DRF, HTML, CSS, SASS, JS, Typescript,
+                  Bootstrap, TailwindCSS. Vue.js, Vue Router, Pinia, Tessy.
+                </span>
+              </li>
+              <li>
+                <h4>Databases</h4>
+                <span class="desc"> PostgreSQL, MySQL, SQLite, Oracle. </span>
+              </li>
+              <li>
+                <h4>Tools</h4>
+                <span class="desc">
+                  Git, GitHub, Docker, Linux, Windows, Figma, Adobe XD, Adobe
+                  Illustrator, Inkscape, GIMP, Microsoft and Google tools.
+                </span>
+              </li>
+              <li>
+                <h4>Methodologies</h4>
+                <span class="desc"> Scrum, Kanban, Agile, V-model. </span>
+              </li>
+              <li>
+                <h4>Soft</h4>
+                <span class="desc">
+                  Team leading, time management, communication, documentation,
+                  problem solving, presentation, work under pressure, and more.
+                </span>
+              </li>
+            </ul>
+          </article>
+
+          <article>
+            <h3>Education</h3>
+            <ul>
+              <li>
+                <div class="flex flex-row justify-between items-end">
+                  <h4>Uniwersytet Śląski w Katowicach</h4>
+                  <span>10/2020 - now</span>
+                </div>
+                <span class="desc">
+                  Applied Computer Science. 6 semester out of 7.
+                </span>
+              </li>
+              <li>
+                <div class="flex flex-row justify-between items-end">
+                  <h4>ZSP nr 6 im. M. Kopernika w Rudzie Śląskiej</h4>
+                  <span>09/2016 - 06/2020</span>
+                </div>
+                <span class="desc">
+                  IT technician. Graduated with avg of 90% score. <br />
+                  Matura exam on top 83% of people from IT field (Python,
+                  Access, Excel).
+                </span>
+              </li>
+            </ul>
+          </article>
+        </div>
+        <div id="agreement" class="text-opacity-60 h-fit">
+          {{ agreement_text }}
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 <style lang="scss">
@@ -154,7 +250,7 @@
   --page-size-scaler: calc(var(--page-scale) * var(--base-page-size));
   --page-height: 0;
   --a4_aspect_ratio: 1.414;
-  --text_height_ratio: calc(10 / 750);
+  --text_height_ratio: calc(9 / 750);
   --page-text-size: calc(var(--page-height) * var(--text_height_ratio));
 }
 @mixin a4-page-set {
@@ -192,12 +288,25 @@
 }
 </style>
 <style scoped lang="scss">
+@media print {
+  .slate-bg {
+    background-color: rgb(30 41 59) !important;
+    print-color-adjust: exact;
+  }
+}
+.slate-bg {
+  background-color: rgb(30 41 59);
+}
+
 div.page {
-  background-color: #fff;
+  background-color: #eee;
   color: #000;
   font-size: var(--page-text-size);
   height: var(--page-height);
   width: var(--page-width);
+  &:not(:first-child) {
+    border-top: 1px solid #000;
+  }
 }
 
 p {
@@ -206,13 +315,16 @@ p {
 .page > article {
   margin: 1em 0;
 }
-
-a {
-  color: var(--color-highligh) !important;
-  text-decoration: underline;
+.page span {
+  font-size: 0.8em;
+  &.desc {
+    line-height: 1;
+  }
 }
 li {
-  margin: 0.3em 0;
+  &:not(:first-child) {
+    margin-top: 0.1em;
+  }
 }
 
 h2,
@@ -225,9 +337,9 @@ h2 {
   font-size: 2em;
 }
 
-article:not(:first-child) {
-  margin: 1em 0;
-}
+// article:not(:first-child) {
+//   margin: 1em 0;
+// }
 
 h3 {
   &:not(:first-child) {
@@ -239,5 +351,8 @@ h3 {
 }
 h4 {
   font-size: 1.2em;
+}
+#agreement {
+  font-size: 0.5em;
 }
 </style>
